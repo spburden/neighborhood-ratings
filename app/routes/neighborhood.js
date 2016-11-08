1b76a18aeeef4c2db107ffbd4bdb385837307121
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-11-08 11:07:21 -0800

    fix merge conflicts.

--- app/routes/neighborhood.js
+++ app/routes/neighborhood.js
@@ -2,15 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url =  'http://api.walkscore.com/score?format=json&lat='+ params.lat + '&lon=' + params.long + '&wsapikey=b54d7a1a14b06657d6ecd2bc9fe8f2ed';
-    return Ember.$.getJSON(url).then(function(responseJSON){
-      console.log(responseJSON);
-     return Ember.RSVP.hash({
-      lat: params.lat,
-      long: params.long,
-      name: params.name,
-    });
-  },
     var neighborhood = this.store.query('neighborhood', {
       orderBy: 'name',
       equalTo: params.name
