9c3720e9d4c63cfbc963b59e9395f8f488c6a88e
Stephen Burden <spburden@hotmail.com>
2016-11-09 11:05:51 -0800

    add transit scores

--- app/components/walk-score.js
+++ app/components/walk-score.js
@@ -7,9 +7,15 @@ export default Ember.Component.extend({
        console.log(responseJSON.walkscore);
        $("#walkScore").text(responseJSON.walkscore);
        $("#walkDescription").text(responseJSON.description);
-      //  var score = responseJSON.walkscore;
-      //  return score;
-      //  return responseJSON;
+     });
+  }),
+  transit: Ember.computed(function() {
+    var url = 'http://transit.walkscore.com/transit/score/?lat=' + this.latLong[0] + '&lon=' + this.latLong[1] + '&city=Portland&state=OR&wsapikey=b54d7a1a14b06657d6ecd2bc9fe8f2ed';
+     return Ember.$.getJSON(url).then(function(responseJSON) {
+       console.log(responseJSON.walkscore);
+       $("#transitScore").text(responseJSON.transit_score);
+       $("#transitDescription").text(responseJSON.description);
+        $("#transitSummary").text(responseJSON.summary);
      });
   }),
 });
