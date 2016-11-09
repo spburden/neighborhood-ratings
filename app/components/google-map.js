442879d5e7a365f3093b1164638e988942121d4b
Eric Krause <krause197@gmail.com>
2016-11-09 14:28:12 -0800

    fixed a few bugs

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -76,26 +76,26 @@ export default Ember.Component.extend({
   },
 
   actions: {
-<<<<<<< HEAD
 
-    showMap(address) {
-      var lat = address.geometry.location.lat;
-      var lng = address.geometry.location.lng;
-      var myLatLng = {lat: lat, lng: lng};
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: myLatLng
-        });
+    // showMapMarker(address) {
+    //   var lat = address.geometry.location.lat;
+    //   var lng = address.geometry.location.lng;
+    //   var myLatLng = {lat: lat, lng: lng};
+    //
+    //     var map = new google.maps.Map(document.getElementById('map'), {
+    //       zoom: 15,
+    //       center: myLatLng
+    //     });
+    //
+    //     var marker = new google.maps.Marker({
+    //       position: myLatLng,
+    //       map: map,
+    //       title: '{{address.formatted_address}}'
+    //     });
+    // },
 
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: '{{address.formatted_address}}'
-        });
-    },
 
-=======
     // getStuff(){
     //   var geo = JSON.parse(localStorage.getItem('../portland.geojson'));
     //   console.log(geo);
@@ -129,6 +129,5 @@ export default Ember.Component.extend({
       };
       map.findMap(container, options);
     }
->>>>>>> master
   }
 });
--- app/models/neighborhood.js
+++ app/models/neighborhood.js
@@ -4,11 +4,11 @@ export default DS.Model.extend({
 
   street: DS.attr('street'),
   city: DS.attr('city'),
-  state: DS.attr('state')
-  name: DS.attr(),
+  state: DS.attr('state'),
+  name: DS.attr(''),
   // zipcode: DS.attr(),
   // latitude: DS.attr('number'),
   // longitude: DS.attr('number'),
-  reviews: DS.hasMany('review', { async: true }),
-  
+  reviews: DS.hasMany('review', { async: true })
+
 });
