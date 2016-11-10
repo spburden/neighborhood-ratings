dadff8ff2e25def95d746de0e126a442e5fbf044
Eric Krause <krause197@gmail.com>
2016-11-10 08:21:34 -0800

    fiddling with demographics

--- app/components/walk-score.js
+++ app/components/walk-score.js
@@ -32,9 +32,29 @@ export default Ember.Component.extend({
       return Ember.$.getJSON(url2).then(function(responseJSON){
         console.log(responseJSON[1][1]);
         $("#state").append(responseJSON[1][1]);
+        $("#state").append(responseJSON[1][1]);
+        $("#state").append(responseJSON[1][1]);
+        $("#state").append(responseJSON[1][1]);
+        $("#state").append(responseJSON[1][1]);
         return Ember.String.htmlSafe('<li>' + 'County: ' + responseJSON[1][0] +'</li>' + '<li>' + 'State: ' + responseJSON[1][1] + '</li>' + '<li>' + 'Total Population: ' + responseJSON[1][2] + '</li>' + '<li>' +
         'People with College Degrees over the age of 25: ' + responseJSON[1][3] + '</li>' + '<li>'+ 'Median Household Income: ' + responseJSON[1][4]);
       });
     });
   }),
 });
+
+// census: Ember.computed(function() {
+//   var url = 'https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x='+ this.latLong[1] + '&y=' + this.latLong[0] +'&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json';
+//   return Ember.$.getJSON(url).then(function(responseJSON) {
+//     //console.log(responseJSON.result.geographies["Census Blocks"]["0"].TRACT);
+//     var tract = responseJSON.result.geographies["Census Blocks"]["0"].TRACT;
+//     var url2 = 'http://api.census.gov/data/2016/pdb/tract?get=County_name,State_name,Tot_Population_CEN_2010,College_ACS_10_14,Med_HHD_Inc_ACS_10_14&for=tract:' + tract + '&in=state:41+county:051&key=d90ad8655f9cb4d3ee0909e5831a4989a51a22ab';
+//     //console.log(tract);
+//     return Ember.$.getJSON(url2).then(function(responseJSON){
+//       console.log(responseJSON[1][1]);
+//       $("#state").append(responseJSON[1][1]);
+//       return Ember.String.htmlSafe('<li>' + 'County: ' + responseJSON[1][0] +'</li>' + '<li>' + 'State: ' + responseJSON[1][1] + '</li>' + '<li>' + 'Total Population: ' + responseJSON[1][2] + '</li>' + '<li>' +
+//       'People with College Degrees over the age of 25: ' + responseJSON[1][3] + '</li>' + '<li>'+ 'Median Household Income: ' + responseJSON[1][4]);
+//     });
+//   });
+// }),
