6880fb3c82b71c024c6cd8e2618f586a72952984
Eric Krause <krause197@gmail.com>
2016-11-09 16:39:19 -0800

    add notes for other demographic variables to possibly use in demographic.js route

--- app/routes/demographic.js
+++ app/routes/demographic.js
@@ -16,6 +16,20 @@ export default Ember.Route.extend({
 
 //variable list at http://api.census.gov/data/2016/pdb/tract/variables.html
 
+//possible variables to use:
+//Aggr_House_Value_ACS_10_14 : Aggregate housing unit value (dollars) in the ACS
+//LAND_AREA : Land area (sq.mi.)
+//pct_Civ_emp_16p_ACS_10_14: percentage of Individuals over 16 Employed
+//pct_College_ACS_10_14 : percentage of individuals over 25 with college degrees
+//pct_Females_ACS_10_14 : percentage of females of total population
+//pct_Males_ACS_10_14 : percentage of females of total population
+//pct_No_Health_Ins_ACS_10_14 : percentage of people with no health insurance
+//pct_Pop_65plus_ACS_10_14 : percentage of population over retirement age
+//pct_Renter_Occp_HU_ACS_10_14 : percentage of population who rent housing
+//Prs_Blw_Pov_Lev_ACS_10_14 : number of people below poverty level
+//
+
+
 //last test query used http://api.census.gov/data/2016/pdb/tract?get=County_name,State_name,Tot_Population_CEN_2010,College_ACS_10_14,Med_HHD_Inc_ACS_10_14,NON_US_Cit_ACS_10_14&for=tract:006402&in=state:41+county:051&key=d90ad8655f9cb4d3ee0909e5831a4989a51a22ab
 
 //back up demographic neighborhoods maps located at https://www.portlandmaps.com/detail/neighborhood/NORTHWEST-DISTRICT-NORTHWEST-INDUSTRIAL/45_did/
